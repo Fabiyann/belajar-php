f73e88d498bbdb9ba097c23d5ba323a63e0e0636
Fabiyan Airlangga <110141320+Fabiyann@users.noreply.github.com>
2022-09-22 01:09:39 +0700

    Add files via upload

---
+++ fabiyan.php
@@ -0,0 +1,18 @@
+<?php 
+      echo " 
+            <marquee> 
+            <h1>Fabiyan Airlangga - XI RPL 2</h1>
+            </marquee> "; 
+            
+            // variable $nama = "Fabiyan Airlangga"; $kelas = "XI RPL 2"; $nilai = 80.02; 
+            
+            echo "Nama : " . $nama; 
+            echo "<br>"; 
+            echo "Kelas : " . $kelas;
+            echo "<br>"; 
+            echo "Nilai : $nilai"; 
+            // Operator Jumlah echo "<br><br>"; $angka1 = 5; $angka2 = 10; $hasil = $angka1 + $angka2; 
+            echo "Hasil dari penjumlahan : $hasil"; // Rumus Luas Segitiga 
+            echo "<br><br>"; $alas = 8; $tinggi = 20; $rumus = 0.5 * $alas * $tinggi; echo "Hasil rumus : $rumus"; // Rumus Luas Lingkaran
+            echo "<br><br>"; $phi = 3.14; $r = 14; $lingkaran = $phi * $r * $r; echo "Hasil Luas : $lingkaran"; // Rumus Volume Balok echo "<br><br>"; $p = 8; $l = 10; $t = 12; $volume_balok = $p * $l * $t; 
+            echo "Volume Balok : $volume_balok";?>
